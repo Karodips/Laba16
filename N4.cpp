7e1676fe79bf8af9063dc7bb9ea46ad6563de74f
Karodips <54810893+Karodips@users.noreply.github.com>
2019-12-30 18:17:39 +0300

    Update N4.cpp

--- N4.cpp
+++ N4.cpp
@@ -5,19 +5,21 @@ using namespace std;
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	int i, n;
+	int i, n, j, kopiya;
 	cin >> n;
-	
-	int* arr = new int[n*2];
+	kopiya = n * 2;
+	int* arr = new int[kopiya];
+	for (i = 0; i < kopiya; i++) arr[i] = 0;
 	for (i = 0; i < n; i++) cin >> arr[i];
 	for (i = 0; i < n; i++) cout << arr[i] << " ";
 	cout << endl;
-	for (i = 0; i < n - 1; i++) {
-		if (arr[i] < 0) { 
-			arr[i + 1] = 0;
-			i++;
-		}
+	for (i = 0; i < n; i++) {
+		if (arr[i] < 0) {
+			for (j = kopiya - 1; j > i+1; j--) arr[j] = arr[j - 1];
 
+			arr[i+1] = 0;
+			n++;
+		}
 	}
 	for (i = 0; i < n; i++) cout << arr[i] << " ";
 	cout << endl;
